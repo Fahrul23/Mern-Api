882aba94d50aa442bc55e80abe60fd2a6247bc31
fahrul23 <ihsanfahrul23@gmail.com>
2020-12-31 10:54:46 +0700

    Basic Router,Request dan Response

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+
+router.use('/product', (req,res,next) =>{
+    
+    res.json({name:"Fahrul Ihsan",email : "Fahrul@gmail.com"})
+    next();
+})
+
+router.use('/price',(req,res,next)=>{
+
+    res.json({price:6000000,})
+    next();
+})
+
+router.get('/customer',(req,res,next)=>{
+    res.json({title:"customer"})
+    next();
+
+})
+
+app.use('/',router);
 
 app.use(function(){
     console.log('server ready')
 })
+
+// GET
 
 app.listen(4000);
