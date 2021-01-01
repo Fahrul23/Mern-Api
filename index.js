f185c73c01a15ea3cac422726e7f5088beacc0c0
fahrul23 <ihsanfahrul23@gmail.com>
2021-01-01 13:52:17 +0700

    Mengatasi CORS ORIGIN API NodeJs

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use((req,res,next) =>{
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
     next();
+    
 });
 
 app.use('/v1/customer',producRoutes);
