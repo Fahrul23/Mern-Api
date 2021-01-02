34de1117c6285e60744911a5fb040e51df64478b
fahrul23 <ihsanfahrul23@gmail.com>
2021-01-02 09:38:18 +0700

    validasi Request Body

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,6 +1,19 @@
+
+const { validationResult } = require('express-validator');
+
 exports.createBlogPost = (req,res,next)=>{
     const title = req.body.title;
     const body = req.body.body;
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        const err = new Error('input value tidak sesuai');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+
 
     const result ={
         message : 'Create Blog Post Success',
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {body} = require('express-validator');
 
 const blogController = require('../controllers/blog');
 // [POST] : /v1/blog/post
-router.post('/post', blogController.createBlogPost);
+router.post('/post', [
+    body('title').isLength({min:5}).withMessage('Input title tidak valid'),
+    body('body').isLength({min:5}).withMessage('Input body tidak valid')],
+    blogController.createBlogPost);
 
 
 module.exports = router;
