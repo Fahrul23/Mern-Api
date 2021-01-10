d6f558a9d8ce25fc8400c55e8ac7f75db9890bb1
fahrul23 <ihsanfahrul23@gmail.com>
2021-01-10 14:47:36 +0700

    upload image use multer

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
 
 app.use(bodyParser.json()); //Type JSON
 
 const authRoutes = require('./src/routes/auth');
 const blogRoutes = require('./src/routes/blog');
+
+const fileStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'images');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().getTime() + '-' + file.originalname)
+    }
+})
+
+const fileFilter= (req,file,cb) =>{
+    if(
+        file.mimetype === 'image/png' || 
+        file.mimetype === 'image/jpg' || 
+        file.mimetype === 'image/jpeg'
+    ){
+        cb(null, true);
+    }else{
+        cb(null,false);
+    }
+}
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname,'images')))
+app.use(multer({storage:fileStorage, fileFilter:fileFilter}).single('image'))
 
 app.use((req,res,next) =>{
     res.setHeader('Access-Control-Allow-Origin','*');
